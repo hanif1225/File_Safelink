669289a2796dc8f4b4ef3041c9bf714c62773ead
hanif1225 <47619034+hanif1225@users.noreply.github.com>
2019-03-30 11:01:35 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
 if(x=="1"){xxx="orders-of-prohibition-relating-to.html"}
-if(x=="2"){xxx="how-to-solve-eating-difficulty-in.html"}a_to_ve[i].href="http://safelink.masoga.net/2018/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="how-to-solve-eating-difficulty-in.html"}a_to_ve[i].href="https://www.safelink.sumberberedukasi.com/2019/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
